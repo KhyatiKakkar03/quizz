c38668392c1cca317c2df77fa6e722c514107317
KhyatiKakkar03 <149072568+KhyatiKakkar03@users.noreply.github.com>
2024-01-18 20:37:56 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+   {
+    numb:1,
+    question:"What does HTML stand for?",
+    answer:2,
+    options:[
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language"
+    ]
+   },
+   {
+    numb:2,
+    question:"What does CSS stand for?",
+    answer:0,
+    options:[
+        "A. Cascading Style Sheet",
+        "B. Cute Style Sheet",
+        "C. Computer Style Sheet",
+        "D. Cascading Styling Sheet"
+    ]
+   },
+   {
+    numb:3,
+    question:"What does PHP stand for?",
+    answer:0,
+    options:[
+        "A. Hypertext Preprocessor",
+        "B. Hometext Programming",
+        "C. Hypertext Programming",
+        "D. Programming Hypertext Preprocessor"
+    ]
+   },
+   {
+    numb:4,
+    question:"What are the types of lists available in HTML?",
+    answer:3,
+    options:[
+        "A. Bulleted , Numbered Lists",
+        "B. Named, Unnamed List",
+        "C. Named, Ordered List",
+        "D. Ordered , Unordered Lists"
+    ]
+   },
+   {
+    numb:5,
+    question:"How many sizes of headers are available in HTML by default?",
+    answer:1,
+    options:[
+        "A. 5",
+        "B. 6",
+        "C. 3",
+        "D. 1"
+    ]
+   },
+];
